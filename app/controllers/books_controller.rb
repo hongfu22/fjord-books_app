b981983069ae40a2cda0df1407abca5947fd5cf7
箭野弘輔 <yayakousuke@MacBook-Air.local>
2022-08-06 10:06:54 +0900

    ページング処理実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    # @books = Book.all
+    @books = Book.page(params[:page]).order(:id).per(5)
   end
 
   # GET /books/1
