be915595836d5576136433a698321aaa5c092f03
箭野弘輔 <yayakousuke@MacBook-Air.local>
2022-08-14 15:34:56 +0900

    deviseの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # deviseのコントローラを使う前に呼ばれるアクション
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # 新規登録時に名前、郵便番号、住所、自己紹介の取得を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postcode, :address, :introduction])
+    # 情報更新時に名前、郵便番号、住所、自己紹介の取得を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :post_code, :address, :introduction])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'books#index'
+  get '/users', to: 'users#index'
+  get '/user/:id', to: 'users#show'
 end
