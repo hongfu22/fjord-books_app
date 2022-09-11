119a06d7e1f67b2cb0d9e247f08d673949ba71e2
箭野弘輔 <yayakousuke@MacBook-Air.local>
2022-09-11 10:52:05 +0900

    画像の拡張子制限+rubocop,erblint対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :image_type
+
+  private
+
+  def image_type
+    errors.add(:image, 'はjpg,png,gif形式のものにして下さい') unless image.content_type.in?(%('image/jpeg image/png image/gif'))
+  end
 end
