c3933aca3d93c7c5433ef9bc6a9625cc62721bb4
箭野弘輔 <yayakousuke@MacBook-Air.local>
2022-09-11 13:57:57 +0900

    不要なファイルの削除+ストロングパラメーターのタイプミス修正+翻訳の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
     # 新規登録時に名前、郵便番号、住所、自己紹介の取得を許可
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name postcode address introduction])
     # 情報更新時に名前、郵便番号、住所、自己紹介の取得を許可
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name post_code address introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postcode address introduction])
   end
 end
