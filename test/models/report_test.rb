6a06fc3eabfa1eaa33b44c8e79580797264f8cc2
箭野弘輔 <jianye.hongfu@gmail.com>
2023-01-15 08:36:49 +0900

    fixturesでのテスト作成完了

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable？' do
+    user = User.new(id: 999, email: 'foo@test.com', name: 'kei')
+    report = user.reports.new(title: 'テスト', content: 'テスト')
+    p report
+    assert_equal true, report.editable?(user)
+  end
+
+  test '#created_at?' do
+    user = User.new(email: 'mike@test.com', name: 'mike')
+    report = Report.new(user_id: user.id, title: 'テスト', content: 'テスト', created_at: Time.zone.now)
+    assert_equal Time.zone.now.to_date, report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'foo@test.com', name: '')
+    assert_equal 'foo@test.com', user.name_or_email
+    user.name = 'FooBar'
+    assert_equal 'FooBar', user.name_or_email
+  end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test '#followed_by?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.follow(she)
+    assert she.followed_by?(me)
+  end
 end
